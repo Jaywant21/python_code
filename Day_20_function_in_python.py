ef3d27f2b6bed46ffcb2cadcda2ed400192c87f2
JAY <148479219+Jaywant21@users.noreply.github.com>
2024-06-04 17:46:34 +0530

    Add files via upload

---
+++ Day_20_function_in_python.py
@@ -0,0 +1,24 @@
+
+def calculategmean(a ,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def greaternum(a,b):
+    if(a > b):
+        print(a)
+    else:
+        print(b)
+
+def islesser(a,b):
+    pass
+
+
+a = 10
+b = 20
+calculategmean(a,b)
+greaternum(a,b)
+
+c = 33
+d = 23
+calculategmean(c,d)
+greaternum(c,d)
